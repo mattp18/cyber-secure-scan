c2f265f72886aaf259ad53ec7c682353e67e9df3
mattp18 <mpuent18@gmail.com>
2023-11-04 23:28:08 -0400

    Modified service class to include constants and added upload controller method

--- src/main/java/com/code4joe/cybersecurescan/web/controller/ScanController.java
+++ src/main/java/com/code4joe/cybersecurescan/web/controller/ScanController.java
@@ -1,7 +1,28 @@
 package com.code4joe.cybersecurescan.web.controller;
 
+import com.code4joe.cybersecurescan.backend.service.MalwareScanService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
+@RequestMapping(path = "/api/v1/scan")
 public class ScanController {
+
+    private final MalwareScanService malwareScanService;
+
+    public ScanController(MalwareScanService malwareScanService) {
+        this.malwareScanService = malwareScanService;
+    }
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> scanFileForMalware(@RequestParam("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(malwareScanService.scanFile(file.getInputStream()));
+    }
 }
