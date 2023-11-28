0b637b877b1d7e26c5fcc3a9f5be2b2dcbe98fcd
mattp18 <mpuent18@gmail.com>
2023-11-28 18:01:35 -0500

    added cross origin temp

--- src/main/java/com/code4joe/cybersecurescan/web/controller/ScanController.java
+++ src/main/java/com/code4joe/cybersecurescan/web/controller/ScanController.java
@@ -4,10 +4,7 @@ import com.code4joe.cybersecurescan.backend.service.MalwareScanService;
 import com.code4joe.cybersecurescan.web.model.ScannedFile;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,6 +20,7 @@ public class ScanController {
     }
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<String> scanFileForMalware(@RequestParam("file") MultipartFile file) throws IOException {
         ScannedFile fileToBeScanned = new ScannedFile();
         fileToBeScanned.setFileName(file.getOriginalFilename());
